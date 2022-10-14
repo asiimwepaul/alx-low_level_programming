adcdab92c8b142bee0cc4c3f0fc587d8d1dd7935
asiimwepaul <asiimwepaul23@gmail.com>
2022-10-14 07:47:17 -0700

    Write a C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - serves as the main entry point for the program
+ * Description: C program that prints a function followed by a new line
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
