b82f429bb43352d9f4957458bf7467303d26c0d7
asiimwepaul <asiimwepaul23@gmail.com>
2022-10-24 13:00:29 -0700

    Reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,9 +11,9 @@ void rev_string(char *s)
 	int l, i;
 	char ch;
 
-	for (i = 0; s[i] != '\0'; ++i)
+	for (l = 0; s[l] != '\0'; ++l)
 		;
-	for (i = 0; i < 1 / 2; ++i)
+	for (i = 0; i < l / 2; ++i)
 	{
 		ch = s[i];
 		s[i] = s[l - 1 - i];
