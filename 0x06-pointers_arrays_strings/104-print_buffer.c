40537a2717f2b57f316b5644191e63ff933014ed
asiimwepaul <asiimwepaul23@gmail.com>
2022-10-26 14:56:04 -0700

    print buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints buffer
+ * @b: buffer to be printed
+ * @size: the number of bytes to be printed from the buffer
+ */
+
+void print_buffer(char *b, int size)
+{
+}
